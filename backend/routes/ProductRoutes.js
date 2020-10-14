6159fd9d5d0e26fa1975e53d3fc4cf7f697b7e6e
Nok <dive877@gmail.com>
2020-10-14 14:49:29 +0400

    Update product routes

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -47,6 +47,60 @@ router.post("/", (req, res) => {
       res.send({ e: e });
     });
 });
+
+/*
+router.post(
+  "/find",
+  
+  (req, res) => {
+    ProductModel.find({ _id: req.product.id })
+      .then((document) => {
+        res.send(document);
+      })
+      .catch((e) => {
+        console.log("e", e);
+        res.send({ e: e });
+      });
+  }
+);
+router.post(
+  "/update",
+  
+  async (req, res) => {
+    const formData = {
+      courseName: req.body.courseName,
+      licence: req.body.licence,
+      price: req.body.price,
+      // password: req.body.password, // will change with bcrypt
+      // photoURL: '' // will be assigned by Cloudinary
+    };
+
+    
+
+    
+
+    // Otherwise, skip encrypting the password
+     
+      ProductModel.findByIdAndUpdate(req.product.id, {
+        $set: formData,
+      })
+        .then((document) => {
+          res.send(document);
+        })
+        .catch((e) => {
+          console.log("e", e);
+          res.send({ e: e });
+        });
+    
+  }
+);
+
+
+
+
+
+
+
 router.post("/update", (req, res) => {
   updateProduct(req, res);
 });
@@ -59,4 +113,5 @@ const updateProduct = (req, res) => {
     doc.save(res.send({ msg: "Changes saved" }));
   });
 };
+*/
 module.exports = router;
